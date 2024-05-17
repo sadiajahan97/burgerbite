a83f6844c620548db013627caccdec15b3a0bbcb
Sadia Iffat Jahan <sadiaiffatjahan@gmail.com>
2024-05-17 13:12:22 +0600

    chore: define MenuItem and Menu interfaces in fetchData.ts

--- src/fetchData.ts
+++ src/fetchData.ts
@@ -1,10 +1,23 @@
+interface MenuItem {
+  id: number;
+  name: string;
+  description: string;
+  image: string;
+  category: string;
+  price: number;
+}
+
+interface Menu {
+  items: MenuItem[];
+}
+
 export default async function () {
   try {
     const response = await fetch('../data/db.json');
     if (!response.ok) {
       throw new Error('There was a problem in fetching the JSON data.');
     }
-    const menu = await response.json();
+    const menu: Menu = await response.json();
     if (!menu) {
       throw new Error('There was a problem in parsing the JSON data.');
     }
