ff8c632ae6420143728ac6eef5f205153d403860
Sadia Iffat Jahan <sadiaiffatjahan@gmail.com>
2024-05-19 22:29:39 +0600

    feat: update MenuItem component to conditionally apply class for shakes and add figcaption

--- src/MenuItem.tsx
+++ src/MenuItem.tsx
@@ -7,10 +7,21 @@ interface ItemType {
 export default function ({ item }: ItemType) {
   return (
     <li role='menu-item'>
-      <img
-        src={`/images/${item.category.replace(' ', '-').toLowerCase()}/${item.image}`}
-        alt={item.image}
-      />
+      <figure>
+        {item.category === 'Shakes' ? (
+          <img
+            className='shakes'
+            src={`/images/${item.category.replace(' ', '-').toLowerCase()}/${item.image}`}
+            alt={item.image}
+          />
+        ) : (
+          <img
+            src={`/images/${item.category.replace(' ', '-').toLowerCase()}/${item.image}`}
+            alt={item.image}
+          />
+        )}
+        <figcaption className='offscreen'>{item.name}</figcaption>
+      </figure>
       <dl>
         <dt>{item.name}</dt>
         <dd>{item.description}</dd>
