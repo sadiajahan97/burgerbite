b58d810d16e31af2c37212055360393e2944e683
Sadia Iffat Jahan <sadiaiffatjahan@gmail.com>
2024-05-20 10:17:45 +0600

    refactor: Menu and App components to pass categories and menu as props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { useState, useEffect } from 'react';
+import { MenuItemType } from './fetchData';
+import fetchData from './fetchData';
 import Menu from './Menu';
 
 export default function () {
+  const [menu, setMenu] = useState<MenuItemType[]>([]);
+  useEffect(() => {
+    async function fetchMenu() {
+      const items = await fetchData();
+      setMenu(items);
+    }
+    fetchMenu();
+  }, []);
+  const categories = Array.from(new Set(menu.map(item => item.category)));
   return (
     <>
-      <Menu />
+      <Menu categories={categories} menu={menu} />
     </>
   );
 }
--- src/Menu.tsx
+++ src/Menu.tsx
@@ -1,18 +1,12 @@
-import { useState, useEffect } from 'react';
 import { MenuItemType } from './fetchData';
-import fetchData from './fetchData';
 import MenuItem from './MenuItem';
 
-export default function () {
-  const [menu, setMenu] = useState<MenuItemType[]>([]);
-  useEffect(() => {
-    async function fetchMenu() {
-      const items = await fetchData();
-      setMenu(items);
-    }
-    fetchMenu();
-  }, []);
-  const categories = Array.from(new Set(menu.map(item => item.category)));
+interface MenuProps {
+  categories: string[];
+  menu: MenuItemType[];
+}
+
+export default function ({ categories, menu }: MenuProps) {
   return (
     <main>
       {categories.map(category => (
