4545702862beb7caaba7deed0693e76fed2826bb
Sadia Iffat Jahan <sadiaiffatjahan@gmail.com>
2024-05-21 14:48:47 +0600

    update

--- src/fetchData.ts
+++ src/fetchData.ts
@@ -13,7 +13,7 @@ interface MenuType {
 
 export default async function () {
   try {
-    const response = await fetch('../data/db.json');
+    const response = await fetch('/data/db.json');
     if (!response.ok) {
       throw new Error('There was a problem in fetching the JSON data.');
     }
